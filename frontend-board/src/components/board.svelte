0b95930804f38805c1a6188b423dc698017d3317
Stanton Zeng <44949352+stantonzeng@users.noreply.github.com>
2023-03-04 01:37:05 -0800

    Added pieces to the side, need to fix ui

--- frontend-board/src/components/board.svelte
+++ frontend-board/src/components/board.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Tile from './board_components/tile.svelte';
-	
+	import Pieces from './board_components/pieces.svelte';
 	/**
 	 * Holds the axis labels and amount. Develops itself
 	 */
@@ -8,6 +8,7 @@
 		constructor(){
 			this.vertSize = 8;
 			this.horzSize = 8;
+			this.numUniquePieces = 12;
 			this.vertAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 			this.horzAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 			this.totalSize = this.vertSize * this.horzSize;
@@ -21,6 +22,10 @@
 			 * @type {{ lbl: string; clr: string; }[]}
 			 */
 			this.boardLabels = [];
+			/**
+			 * @type {number[]}
+			 */
+			this.uniquePiecesList = [];
 
 			for (let i = 0; i < this.vertAxis.length; i++) {
 				for(let j = 0; j < this.horzAxis.length; j++){
@@ -29,6 +34,10 @@
 					this.boardLabels[i*this.vertSize + j] = {lbl: _lbl, clr: _clr};
 				}
 			}
+			for (let i = 0; i < this.numUniquePieces; i++) {
+				this.uniquePiecesList[i] = i;
+				
+			}
 		}
 	};
 	
@@ -36,25 +45,65 @@
 	
 </script>
 
-
-<div class = "board">
-	<!--This if statement is used just to remove the undefined board1 error-->
-	{#if board_1.boardLabels}
-		<!--Loop through the boardlabels and assign them to each tile-->
-		{#each board_1.boardLabels as bl}
-			<Tile nme = {bl.lbl} clr = {bl.clr}/>
-		{/each}
-	{/if}
-		
+<div class = "board_and_pieces">
+	<div class = "board">
+		<!--This if statement is used just to remove the undefined board1 error-->
+		{#if board_1.boardLabels}
+			<!--Loop through the boardLabels and assign them to each tile-->
+			{#each board_1.boardLabels as bl}
+				<Tile nme = {bl.lbl} clr = {bl.clr}/>
+			{/each}
+		{/if}
+	</div>
+	<!--This holds the pieces grid and centers it-->
+	<div class = "pieces">
+		<!--Grids the pieces on the side-->
+		<div class = "pieces_grid">
+			{#if board_1.uniquePiecesList}
+				{#each board_1.uniquePiecesList as pl}
+					<Pieces ind = {pl}/>
+				{/each}
+			{/if}
+		</div>
+	</div>	
 </div>
 
 <style>
+	.board_and_pieces{
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr; 
+		grid-template-rows: 1fr; 
+		gap: 0px 0px; 
+		grid-template-areas: 
+			". board pieces"; 
+	}
 	.board {
+		grid-area: board;
 		display: grid;
 		place-content: center;
 		grid-template-columns: repeat(8, 100px);
 		grid-template-rows: repeat(8, 100px);
 		grid-column-gap: 0px;
 		grid-row-gap: 0px;
+	}
+	.pieces {
+		grid-area: pieces;
+		display: flex;
+		place-content: center;
+		justify-content: center;
+    	align-items: center;
+	}
+	.pieces_grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr; 
+		grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr; 
+		gap: 0px 0px; 
+		/* grid-template-areas: 
+			". ."
+			". ."
+			". ."
+			". ."
+			". ."
+			". .";  */
 	}
 </style>
---
+++ frontend-board/src/components/board_components/pieces.svelte
@@ -0,0 +1,38 @@
+<script>
+    import BlackBishop from './pieces/Chess_bdt60.png' 
+    import WhiteBishop from './pieces/Chess_blt60.png' 
+    import BlackKing from './pieces/Chess_kdt60.png' 
+    import WhiteKing from './pieces/Chess_klt60.png'
+    import BlackKnight from './pieces/Chess_ndt60.png' 
+    import WhiteKnight from './pieces/Chess_nlt60.png' 
+    import BlackPawn from './pieces/Chess_pdt60.png' 
+    import WhitePawn from './pieces/Chess_plt60.png'
+    import BlackQueen from './pieces/Chess_qdt60.png' 
+    import WhiteQueen from './pieces/Chess_qlt60.png' 
+    import BlackRook from './pieces/Chess_rdt60.png' 
+    import WhiteRook from './pieces/Chess_rlt60.png'
+
+    export let ind;
+
+    const pieceHolder = [BlackBishop, WhiteBishop, BlackKing, WhiteKing, BlackKnight,
+    WhiteKnight, BlackPawn, WhitePawn, BlackQueen, WhiteQueen, BlackRook, WhiteRook];
+    
+    const pieceHolderAlt = ["Black Bishop", "White Bishop", "Black King", "White King", "Black Knight",
+    "White Knight", "Black Pawn", "White Pawn", "Black Queen", "White Queen", "Black Rook", "White Rook"];
+
+    const piece = {
+        pieceType: pieceHolder[ind],
+        pieceAlt: pieceHolderAlt[ind]
+    }
+</script>
+
+<div class = "pieces_section">
+    <img src = {piece.pieceType} alt = {piece.pieceAlt}/>
+</div>
+
+<style>
+    .pieces_section{
+        display: flex;
+        background-color: bisque;
+    }
+</style>
